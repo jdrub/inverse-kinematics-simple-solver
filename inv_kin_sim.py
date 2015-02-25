9dc1bba3039d20c6edbb772bae52775281797c8d
jdrub <j.drubin94@gmail.com>
2015-02-25 02:58:26 -0500

    commented out quick debug print statements

--- inv_kin_sim.py
+++ inv_kin_sim.py
@@ -39,9 +39,9 @@ t2 = math.pi
 t3 = math.acos(x2/(math.sqrt(math.pow(x2,2) + math.pow(z2,2))))
 
 
-print "t0: " + str(t0)
-print "t1: " + str(t1)
-print "t2: " + str(t2)
+# print "t0: " + str(t0)
+# print "t1: " + str(t1)
+# print "t2: " + str(t2)
 
 p0x = 0
 p0y = 0
@@ -50,14 +50,14 @@ p0z = 0
 p1x = l1*math.cos(t0)
 p1y = l1*math.sin(t0)
 p1z = p1x*math.sin(t3)
-print "p1x: " + str(p1x)
-print "p1y: " + str(p1y)
+# print "p1x: " + str(p1x)
+# print "p1y: " + str(p1y)
 
 p2x = p1x + l2*math.cos(- (math.pi - t1 - t0))
 p2y = p1y + l2*math.sin(- (math.pi - t1 - t0))
 p2z = p2x*math.sin(t3)
-print "p2x: " + str(p2x)
-print "p2y: " + str(p2y)
+# print "p2x: " + str(p2x)
+# print "p2y: " + str(p2y)
 
 # p3x = p2x + l3*math.cos(math.pi - t2 - t1)
 # p3y = p2y + l3*math.sin(math.pi - t2 - t1)
@@ -77,6 +77,3 @@ ax.scatter([p3z],[p3x],[p3y],color="g",s=100)
 
 
 plt.show()
-
-
-
